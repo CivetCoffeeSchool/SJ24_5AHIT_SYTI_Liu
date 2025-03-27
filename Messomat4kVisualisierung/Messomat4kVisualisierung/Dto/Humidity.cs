b51c04969a52e0c2c0646496ba6f16edead4cd79
CivetCoffeeSchool <132644816+CivetCoffeeSchool@users.noreply.github.com>
2025-03-27 08:33:47 +0100

    VisualizerFinal

--- Messomat4kVisualisierung/Messomat4kVisualisierung/Dto/Humidity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Messomat4kVisualisierung.Dto;
-
-public class Humidity:AGraphData
-{
-    public Humidity(float data, int serialNumber) : base(data, serialNumber)
-    {
-    }
-}
--- Messomat4kVisualisierung/Messomat4kVisualisierung/Dto/Temperature.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Messomat4kVisualisierung.Dto;
-
-public class Temperature : AGraphData
-{
-    public Temperature(float data, int serialNumber) : base(data, serialNumber)
-    {
-    }
-}
